35dbb4ee56cdaa0e10d47ed307bd5bd768a3ae30
Neftali Forero <neftaiof@gmail.com>
2021-02-20 02:40:03 -0500

    Styling button to upload xml invoice, part 1.

--- components/InvoiceValidator/InvoiceValidator.tsx
+++ components/InvoiceValidator/InvoiceValidator.tsx
@@ -1,11 +1,12 @@
 import Description from "@components/Description";
+import InvoiceReader from "@components/InvoiceReader";
 import React from "react";
 
 const InvoiceValidator = () => {
   return (
     <div className="container">
       <Description />
-      This is good....ss
+      <InvoiceReader />
     </div>
   );
 };
